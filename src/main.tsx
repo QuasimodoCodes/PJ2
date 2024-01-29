08ad87768e8b63a59e06745a722559ec230b4eb6
Victor Hugo Figueroa <102181741+SpookyBugie@users.noreply.github.com>
2024-01-29 15:01:25 +0100

    added css

---
+++ src/main.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { MapApplication } from "./components/application/MapApplication";
+
+const root = ReactDOM.createRoot(document.getElementById("root")!);
+
+root.render(<MapApplication />);
